958c9ff16e783efda20219e787d55ff2bd1bbd8e
hmawari <nguyenphan632009@gmail.com>
2025-08-02 13:06:22 +0700

    Update 🍁🌻✨.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    titleWeb: 'Gửi Nam Phonggg',
+    titleWeb: "Gửi Nam Phonggg",
     introTitle: '🍁🍂',
-    introDesc: `hellooo, 2/8 gòi nè anh có biết hôm nay là ngày gì khomm:))_`,
+    introDesc: `hellooo, 2/8 gòi nè anh có biết hôm nay là ngày gì khomm:))`,
     btnIntro: ' ýe đúng òi nay là sinh nhật A̶n̶h̶ i̶u̶ Nam Phong đó ^^HiHi^^',
     title: '18 tuổi rồi, sắp phải ra ngoài var với xã hội gòi:)) nên là chúc gặp nhiều may mắn, bớt đen lại:) với cạ là phải vui vẻ lên nè, mở lòng ra đón nhận, tiếp thu những cái mới mẻ nhe chúc anh thích nghi được tốt với cuộc sống mới phải tự lập ở đại học. Học giỏi nhe i̶u̶',
     desc: 'Thoát web là phải cố gắng thực hiện tốt mấy lời em chúc nhee',
@@ -11,6 +11,9 @@ const CONFIG = {
     btnReply: 'Gửi',
     reply: 'đẹt ti ni đó hẹ hẹ🤡🥰:))',
     mess: 'aww em biết mà✨:))',
+    finalDesc: 'vì cũng có căn dữ lắm mới gặp được nhau nên là dù có thế nào thì cũng không được sủi nhe🌻',
+    btnYes: 'chắc chắn òi:]]',
+    btnNo: 'hên xui😞',
     messDesc: 'Tối nay, mình chơi game, xem phim nhaaa.',
-    btnAccept: 'Okiiiii lun <3',
-    title: '🌻'
+    btnAccept: 'Okiiiii lun <3'
+}
